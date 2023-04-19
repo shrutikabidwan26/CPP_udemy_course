c96df9cbd4fa2e5fb4eef744e3a2bd195dfdb228
shrutikabidwan26 <shrutikabidwan@gmail.com>
2023-04-19 11:25:39 +0530

    S25_LambdaFunction_A2

---
+++ S25_LambdaFunction_A2/src/S25_LambdaFunction_A2.cpp
@@ -0,0 +1,67 @@
+//============================================================================
+// Name        : S25_LambdaFunction_A2.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description :Capture Lists
+//============================================================================
+
+#include <iostream>
+
+
+int main()
+{
+
+
+	//Capture lists
+/*
+     double a{10};
+     double b{20};
+
+ 
+
+     auto func = [a,b]()
+	{
+         std::cout  << "a + b : " << a + b << std::endl;
+     };
+     func();
+     */
+
+	//Capturing by value
+/*
+     int c{42};
+
+ 
+
+     auto func = [c]()
+	{
+         std::cout << "Inner value : " << c << " &inner : " <<&c <<  std::endl;
+     };
+
+ 
+
+     for(size_t i{} ; i < 5 ;++i)
+	{
+         std::cout << "Outer value : " << c << " &outer : " << &c << std::endl;
+         func();
+         ++c;
+     }
+     */
+
+	//Capture by reference
+	int c{42};
+
+	auto func = [&c]()
+			{
+			std::cout << "Inner value : " << c << " &inner : " <<&c <<  std::endl;
+			};
+
+	for(size_t i{} ; i < 5 ;++i)
+			{
+			std::cout << "Outer value : " << c << " &outer : " << &c << std::endl;
+			func();
+			++c;
+			}
+
+		return 0;
+}
